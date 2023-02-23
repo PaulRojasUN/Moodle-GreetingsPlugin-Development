844c963fb06d4510e1d1d6212627337e31ef5666
PaulRojasUN <paul.rojas@correounivalle.edu.co>
2023-02-23 15:52:33 -0500

    Add plugin link to navigation

--- lib.php
+++ lib.php
@@ -61,3 +61,41 @@ function local_greetings_get_greeting($user) {
 
     return get_string($langstr, 'local_greetings', fullname($user));
 }
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in
+ * the navigation tree.
+ * @return void
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    $frontpage->add(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+}
+
+/**
+ * Insert a link to index.php on the site global navigation menu.
+ *
+ * @param global_navigation $root node
+ * @return void
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    $node = navigation_node::create(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+
+    $node->showinflatnavigation = true;
+    $root->add_node($node);
+}
